4e4d92a7aafd575188143e9415d57db903e429db
EzyhB <ezyhbmail@gmail.com>
2022-09-14 17:46:04 +0100

    feature bar fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,55 +9,70 @@ import css from "../styles/homepage.module.css";
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth="xl" className={css.bgContainer}>
+    <Container
+      maxWidth="xl"
+      className={css.bgContainer}
+      background="background"
+    >
       <Container maxWidth="md" className={css.searchBarPadding}></Container>
       <Container maxWidth="md" className={css.searchBar}>
         <InputField></InputField>
       </Container>
       <GridContainer>
         <GridItem md="four" className={css.featureGrid}>
-          <Typography className={css.featureHeader}>Great Value</Typography>
-          <Image
-            src="/images/a-star-car-sales-great-value-icon.png"
-            height={"205px"}
-            width={"205px"}
-            className={css.featureImage}
-          />
-          <Typography>
+          <Typography className={css.featureText}>Great Value</Typography>
+          <Container className={css.featureImageContainer}>
+            <img
+              src="/images/a-star-car-sales-great-value-icon.png"
+              height={"50%"}
+              width={"50%"}
+              className={css.featureImage}
+            />
+          </Container>
+
+          <Typography className={css.featureText}>
             Browse our wide range of cars from the comfort of your sofa,
             whatever your budget.
           </Typography>
         </GridItem>
         <GridItem md="four">
-          <Typography className={css.featureHeader}>
-            {" "}
+          <Typography className={css.featureText}>
             High-Quality Used Cars
           </Typography>
-          <Image
-            src="/images/a-star-car-sales-HIGH-QUALITY-USED-CAR-icon.png"
-            height={"205px"}
-            width={"205px"}
-          />
-          <Typography>
+          <Container className={css.featureImageContainer}>
+            <img
+              src="/images/a-star-car-sales-HIGH-QUALITY-USED-CAR-icon.png"
+              height={"50%"}
+              width={"50%"}
+              className={css.featureImage}
+            />
+          </Container>
+          <Typography className={css.featureText}>
             We own our cars and each one is fully reconditioned, so quality
             comes guaranteed.
           </Typography>
         </GridItem>
         <GridItem md="four">
-          <Typography className={css.featureHeader}>
+          <Typography className={css.featureText}>
             Flexible Finance Options
           </Typography>
-          <Image
-            src="/images/a-star-car-sales-flexible-finance-icon.png"
-            height={"205px"}
-            width={"205px"}
-          />
-          <Typography>
+          <Container className={css.featureImageContainer}>
+            <img
+              src="/images/a-star-car-sales-flexible-finance-icon.png"
+              height={"50%"}
+              width={"50%"}
+              className={css.featureImage}
+            />
+          </Container>
+          <Typography className={css.featureText}>
             Quick, easy and entirely online, get instant approvals for monthly
             finance payments.
           </Typography>
         </GridItem>
       </GridContainer>
+      <Container maxWidth="none">
+        <Container maxWidth="md"></Container>
+      </Container>
     </Container>
   );
 };
