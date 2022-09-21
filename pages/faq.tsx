f733e82ef05e6320ae9fc1066e8b5eabec8bf621
EzyhB <ezyhbmail@gmail.com>
2022-09-21 16:44:28 +0100

    added FAQ mock db

---
+++ pages/faq.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container } from "../components/styles/Container";
+import { Typography } from "../components/styles/Typography";
+import css from "../styles/faq.module.css";
+
+export default function faq() {
+  return (
+    <Container maxWidth="xl">
+      <Container
+        maxWidth="xl"
+        background="primary"
+        className={css.headerContainer}
+      >
+        <Typography variant="h4" className={css.headdersPadding}>
+          A-Star Support
+        </Typography>
+        <Typography className={css.textPadding}>
+          Have a question? Whatever you need, we’re here to help.
+        </Typography>
+        <Typography>
+          We’re here to help 7 days a week from 8am-8pm. You can chat to us,
+          WhatsApp us on 07984 395 125 or call 020 3901 3488.
+        </Typography>
+      </Container>
+    </Container>
+  );
+}
