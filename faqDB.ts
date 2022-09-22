dbdd907dbd34dc7cc3df700e693f27dab1375b06
EzyhB <ezyhbmail@gmail.com>
2022-09-22 17:18:48 +0100

    ids added to FAQ db and edited spacing

--- faqDB.ts
+++ faqDB.ts
@@ -1,34 +1,40 @@
 const faqDB = [
   {
+    id: 1,
     question: "Can I test drive the car?",
     answer:
-      "You can’t test drive our cars, but our 7-Day Money Back Guarantee is more comprehensive than a typical test drive. We give you up to a week to get to know your car and to make sure it's right for you. If you change your mind for any reason within 7 days of delivery or collection, just let us know and we’ll pick it up and give you a full refund, including the admin fee. We can’t accept a return if the car has been modified, been in an accident or is damaged or altered from the condition it was received in. If you drive the car over the 250 mile limit during the first 7 days, every additional mile will cost £1 per mile alongside deductions for any damage incurred.",
+      "You can’t test drive our cars, but our 7-Day Money Back Guarantee is more comprehensive than a typical test drive. We give you up to a week to get to know your car and to make sure it's right for you. \n\nIf you change your mind for any reason within 7 days of delivery or collection, just let us know and we’ll pick it up and give you a full refund, including the admin fee. \n\nWe can’t accept a return if the car has been modified, been in an accident or is damaged or altered from the condition it was received in. \n\nIf you drive the car over the 250 mile limit during the first 7 days, every additional mile will cost £1 per mile alongside deductions for any damage incurred.",
   },
   {
+    id: 2,
     question: "Can I negotiate the price of A-Star cars?",
     answer:
       "There’s no need. We offer a transparent and haggle-free experience. The prices listed for each car are our best prices and are as competitive as possible. The price you see is the price that you’ll pay for your car.",
   },
   {
+    id: 3,
     question: "Can I see a car before I buy it?",
     answer:
       "Feel free to visit us at any time at our location found on the website.",
   },
-  { question: "How do I pay for the car?", answer: "test" },
+
   {
+    id: 4,
     question: "Where do A-Star cars come from?",
     answer:
       "We own all our used cars. Our buying team purchase high-quality cars from a variety of sources, including auctions, dealerships, leasing companies and manufacturers. We also sell cars that have been part exchanged by our customers. Before we purchase a car, we thoroughly inspect it to make sure it meets our highest standards. ",
   },
   {
-    question: "Does Cazoo buy vehicles with insurance issues?",
+    id: 5,
+    question: "Does A-Star Car Sales buy vehicles with insurance issues?",
     answer:
-      "At Cazoo, we won’t buy or sell a vehicle with insurance issues. This means that we won’t buy or sell a vehicle if it’s been categorised as A, B, S or N by the Motor Insurers Anti-Fraud and Theft Register (MIAFTR). What are categories A, B, S and N? These categories are how insurance providers define the different levels of accident damage to a vehicle. Category A: The vehicle has suffered extensive damage and is no longer safe to drive. Category B: The vehicle has had significant damage but some parts may be salvageable. It’s unsafe to drive but reclaimed parts can be used in roadworthy vehicles. Category S (formerly C): The vehicle has had some structural damage. Until the vehicle has been professionally repaired, it isn’t safe to drive. Category N (formerly D): The damage to the vehicle is likely cosmetic or electrical and may not be safe to repair. The damage could affect the braking, steering or other safety related parts, meaning the vehicle may not be safe to drive.",
+      "At A-Star Car Sales, we won’t buy or sell a vehicle with insurance issues. This means that we won’t buy or sell a vehicle if it’s been categorised as A, B, S or N by the Motor Insurers Anti-Fraud and Theft Register (MIAFTR). \n \nWhat are categories A, B, S and N? These categories are how insurance providers define the different levels of accident damage to a vehicle. \n\nCategory A: The vehicle has suffered extensive damage and is no longer safe to drive. \n\nCategory B: The vehicle has had significant damage but some parts may be salvageable. It’s unsafe to drive but reclaimed parts can be used in roadworthy vehicles. \n\nCategory S (formerly C): The vehicle has had some structural damage. Until the vehicle has been professionally repaired, it isn’t safe to drive. \n\nCategory N (formerly D): The damage to the vehicle is likely cosmetic or electrical and may not be safe to repair. The damage could affect the braking, steering or other safety related parts, meaning the vehicle may not be safe to drive.",
   },
   {
+    id: 6,
     question: "What documents will I receive with my used car?",
     answer:
-      "When we hand over your Cazoo car, you'll receive its service history alongside a welcome pack and a free selection of car goodies.",
+      "When we hand over your A-Star car, you'll receive its service history alongside a welcome pack and a free selection of car goodies.",
   },
 ];
 
