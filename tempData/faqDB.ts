3e077a6e4bf10630901c216e8e36635a016e2850
EzyhB <ezyhbmail@gmail.com>
2022-11-11 14:02:43 +0000

    changed FAQ around for Can i test drive

--- tempData/faqDB.ts
+++ tempData/faqDB.ts
@@ -3,7 +3,7 @@ const faqDB = [
     id: 0,
     question: "Can I test drive the car?",
     answer:
-      "You can test drive our cars, infact, we always recommend you test drive the car before purchasing it. We also give you up to a week to get to know your car and to make sure it's right for you. *If you change your mind for any reason within 7 days of delivery or collection, just let us know and we’ll pick it up and give you a full refund, including the admin fee. *We can’t accept a return if the car has been modified, been in an accident or is damaged or altered from the condition it was received in. *If you drive the car over the 150 mile limit during the first 7 days, every additional mile will cost £1 per mile alongside deductions for any damage incurred.",
+      "You can test drive our cars, infact, we always recommend you test drive the car before purchasing it. We also give you up to a week to get to know your car and to make sure it's right for you. *If you change your mind for any reason within 7 days of delivery or collection, just let us know and we’ll pick it up and give you a full refund (cost of collection is free for the first 10 miles, additional charges may apply for longer distances). *We can’t accept a return if the car has been modified, been in an accident or is damaged or altered from the condition it was received in. *If you drive the car over the 100 mile limit during the first 7 days, every additional mile will cost £1 per mile alongside deductions for any damage incurred.",
   },
   {
     id: 1,
